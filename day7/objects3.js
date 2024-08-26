348eead91fa8a764ba17c1a380498efebd6b967b
Tiyasha-devB <tiyasha.11f.22082003@gmail.com>
2024-08-26 07:49:20 +0000

    JSON doc

---
+++ day7/objects3.js
@@ -0,0 +1,15 @@
+// de-structuring and JSON API
+const course = {
+    name: "js_hindi",
+    price: "999",
+    cIns: "hitesh"
+}
+const {cIns:instructor} = course //destructure
+//console.log(cIns);
+console.log(instructor);
+
+
+/*JSON STRUCTURE:
+API KEY: https://randomuser.me/api/
+[FROM RANDOM USER ME]
+JSON FORMATTER*/
