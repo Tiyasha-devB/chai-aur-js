a967567bfb1cb2fd3820321f5e920981fb40bc66
Tiiyasha Banerjee <tiyasha.11f.22082003@gmail.com>
2024-10-01 08:13:31 +0000

    p3

--- bmi calculator/calculator.js
+++ bmi calculator/calculator.js
@@ -5,14 +5,23 @@ form.addEventListener('submit', function(e){
     const weight = parseInt(document.querySelector('#weight').value)
     const results = document.querySelector('#results')
     
+    const bmi = (weight/((height/100)**2)).toFixed(2);
+
     if(height === '' || height < 0 || isNaN(height)){
         results.innerHTML = `Please give a valid height. ${height} is not valid`;
     }
-    if(weight === '' || weight < 0 || isNaN(weight)){
-        results.innerHTML = `Please give a valid height. ${weight} is not valid`;
-    }
-    else{
-        const bmi = (weight/((height*weight)/10000)).toFixed(2);
-        results.innerHTML = `<span>${bmi}</spam>`;
+    else if(bmi<=16.0){
+        results.innerHTML=`Under Weight :${bmi}`;
+    }else if(bmi>16.0 && bmi<=25.0){
+            results.textContent=`Normal Weight:${bmi}`;
+    }else if(bmi>25.0 && bmi<=35.0){
+            results.textContent=`Over Weight : ${bmi}`;
+    }else if(bmi>30.0 && bmi<=35.0){
+        results.textContent=`Obese Class I: ${bmi}`;
+    }else if(bmi>35.0 && bmi<=40.0){
+        results.textContent=`Obese Class II: ${bmi}`;
+    }else{
+        results.textContent=`Obese Class III: ${bmi}`;
     }
 })
+
