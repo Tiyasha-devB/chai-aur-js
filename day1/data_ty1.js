af827f5f76ce2b40ca649a5c1cad4b7aa50e097a
Tiiyasha Banerjee <tiyasha.11f.22082003@gmail.com>
2024-10-16 08:06:44 +0000

    ty

--- day1/data_ty1.js
+++ day1/data_ty1.js
@@ -1,15 +1,18 @@
+/**JavaScript (JS) is a lightweight interpreted (or just-in-time compiled) programming language with first-class functions. 
+JavaScript is a prototype-based, multi-paradigm, single-threaded, dynamic language, supporting object-oriented, imperative, 
+and declarative (e.g. functional programming) styles.*/
+
 //alert(3+3) using nodejs not browser
 //tc39.es : ECMA script standards
 
 let name = "Tiyasha" //string 
-let age = 20 //number
+let age = 20 //number (NaN -> number type)
 let isLoggedIn = false //boolean
 //null => standalone value
 // undefined => empty
 // symbol => uniqueness
-
 //object
 
 console.log(typeof name);
-console.log(typeof(null)); //object
-console.log(typeof(undefined)); //undefined
+console.log(typeof (null)); //object
+console.log(typeof (undefined)); //undefined
