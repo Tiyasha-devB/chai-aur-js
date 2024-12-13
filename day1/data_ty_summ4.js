e2ad382e286d4a6bba1fa14beda382d8fa3cd844
Tiyasha Banerjee <tiyasha.11f.22082003@gmail.com>
2024-12-13 07:14:30 +0000

    new1

--- day1/data_ty_summ4.js
+++ day1/data_ty_summ4.js
@@ -2,10 +2,14 @@
 
 const id = Symbol('123')
 const id2 = Symbol('123')
-console.log(id == id2); // false as the value changes
+console.log(id == id2); 
+// In JavaScript, Symbols are unique identifiers. 
+// Even if they're created with the same description, they represent different objects in memory. 
+// This is why comparing two Symbols with the same description using == will always result in false.
+
 
 // non-primitive - array, objects, functions
-const array1 = ["tiyasha","tanbir","tamanna","trisha"] //object
+const array1 = ["tiyasha","tanbir","tamanna","trisha"] //array object 
 const obj1 = {
     name: "tiyasha",
     age: 22,
